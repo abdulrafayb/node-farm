fe260e2688599b39a1fa3d8d4ae51abd4698ee28
abdulrafayb <abdulrafay200066@gmail.com>
2026-02-23 02:47:16 +0500

    building a very simple api

--- index.js
+++ index.js
@@ -35,14 +35,22 @@ console.log('Will read file!'); */
 ///////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   // console.log(req);
   const pathName = req.url;
 
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the overview');
+    res.end('This is the OVERVIEW');
   } else if (pathName === '/product') {
-    res.end('This is the product');
+    res.end('This is the PRODUCT');
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
